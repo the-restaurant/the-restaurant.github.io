a53078994dd07b847407f38d660d5ba0a6fead78
the-restaurant <88305145+the-restaurant@users.noreply.github.com>
2021-08-04 17:03:29 -0700

    css improvements, some refactoring

--- webconsole.js
+++ webconsole.js
@@ -1,33 +1,32 @@
-// web console
+//web console
 
-var cin = "";
-var textHist = "";
-var newText = "";
-var nextDirective = "";
-var commandSeparator = " ";
-var outputSeparator = "\n";
-var directiveSeparator = "\n";
-var buttonArray = ["A","B","C","D"];
+let cin = "";
+let textHist = "";
+let newText = "";
+let nextDirective = "";
+let commandSeparator = " ";
+let outputSeparator = "\n";
+let directiveSeparator = "\n";
+const buttonArray = ["A", "B", "C", "D"];
 
 function toConsole() {
-	cin = document.getElementById("consoleIn").value.toString();
-	//this function defined in the specific program js, not webconsole.js
-	thisProgram();
+  cin = document.getElementById("consoleIn").value.toString();
+  //this function defined in the specific program js, not webconsole.js
+  thisProgram();
 }
 
 function updateConsole() {
-	if(textHist == "") {
-		textHist = newText;
-	} else {
-		textHist += commandSeparator+cin+outputSeparator+newText+directiveSeparator+nextDirective;
-	}
-	document.getElementById("consoleOut").innerHTML = textHist;
-	document.getElementById("consoleOut").scrollTop = document.getElementById("consoleOut").scrollHeight;
+  if (newText.length <= 0) {
+    return;
+  }
+  textHist = textHist.length > 0 ? textHist + commandSeparator + cin + outputSeparator + newText + directiveSeparator + nextDirective : newText + directiveSeparator + nextDirective;
+  document.getElementById("consoleOut").innerHTML = textHist;
+  document.getElementById("consoleOut").scrollTop = document.getElementById("consoleOut").scrollHeight;
 }
 
 function inConsoleBoxSubmit(e) {
-	e.preventDefault();
-	toConsole();
+  e.preventDefault();
+  toConsole();
 }
 
-document.getElementById("consoleForm").addEventListener("submit",inConsoleBoxSubmit);
+document.getElementById("consoleForm").addEventListener("submit", inConsoleBoxSubmit);
